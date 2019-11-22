29b55175ab242cf96cc78f3e016ed70d4bc55903
Luke Kaalim <lukekaalim@gmail.com>
2019-11-22 17:03:07 +1100

    Clear console for each test run

--- test.js
+++ test.js
@@ -1,12 +1,15 @@
 // @flow strict
 const { assert, colorReporter, unicodeReporter } = require('@lukekaalim/test');
 const { testJavascript } = require('./src/javascript.test');
+const { testRefinement } = require('./src/refinement.test');
 const { writeFile } = require('fs').promises;
 
 const test = async () => {
   try {
+    console.log('\033[H\033[2J')
     const assertion = assert('@lukekaalim/type should provide a safe typing language', [
       await testJavascript(),
+      await testRefinement(),
     ]);
     console.log(colorReporter(assertion));
     await writeFile('./test.log', unicodeReporter(assertion));
